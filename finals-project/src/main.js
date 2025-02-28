82d8133ea432f5e7a639a802ac7bb349ecd16429
Marianna Ellise Lama <153097209+mariannelama@users.noreply.github.com>
2025-02-28 03:41:32 +0000

    sFV

--- finals-project/src/main.js
+++ finals-project/src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
+app.mount('#app')
