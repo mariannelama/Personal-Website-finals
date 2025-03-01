836bb63aeea427fa7b7a4d32026cc828ab01cdf6
Marianna Ellise Lama <153097209+mariannelama@users.noreply.github.com>
2025-03-01 06:08:22 +0000

    changes

--- finals-project/src/App.vue
+++ finals-project/src/App.vue
@@ -1,35 +1,8 @@
 <template>
-  <div id="app">
-    <button @click="toggleComments" class="btn">Get to know me</button>
-
-    <CommentForm v-if="showComments" @add-comment="addComment" />
-    <Comment v-if="showComments" :comments="comments" />
-  </div>
-</template>
-
-<script>
-import Comment from './components/Comment.vue';
-import CommentForm from './components/CommentForm.vue';
-
-export default {
-  components: { Comment, CommentForm },
-  data() {
-    return {
-      showComments: false,
-      comments: []
-    };
-  },
-  methods: {
-    toggleComments() {
-      this.showComments = !this.showComments;
-    },
-    addComment(commentData) {
-      this.comments.push(commentData);
-    }
-  }
-};
-</script>
-
-<style>
-/* Add any necessary styles here */
-</style>
+    <comment-form/>
+    <comment/>
+     
+  
+  </template>
+  
+  <script></script>
