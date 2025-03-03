9b0a10eb55581b1352db79163e5e613e263cdb8e
Marianna Ellise Lama <153097209+mariannelama@users.noreply.github.com>
2025-03-03 09:15:09 +0000

    kk

--- finals-project/src/App.vue
+++ finals-project/src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <comment-section/>
+  <section>
+    <div id="appContainer" class="btn" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="1800">
+      <button @click="toggleContent">Get to know me</button>
+      <div v-if="showForm" id="app">
+        <CommentSection />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
-import CommentSection from './components/CommentSection.vue'; 
+import { ref } from 'vue';
+import CommentSection from './components/CommentSection.vue';
+
+const showForm = ref(false);
+
+function toggleContent() {
+  showForm.value = !showForm.value;
+}
 </script>
