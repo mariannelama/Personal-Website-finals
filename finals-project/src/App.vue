e7af1dd0c1780ef763a34ae806ad0f974fca5341
Marianna Ellise Lama <153097209+mariannelama@users.noreply.github.com>
2025-03-03 04:38:51 +0000

    changes

--- finals-project/src/App.vue
+++ finals-project/src/App.vue
@@ -1,51 +1,30 @@
-<template>
-  <div class="p-4">
-    <!-- Button to toggle comment section -->
-    <button @click="toggleComments" class="bg-purple-600 text-white font-bold px-6 py-3 rounded-lg">
-      Get to Know Me
-    </button>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
 
-    <!-- Comment Section (Hidden Until Clicked) -->
-    <div v-if="showComments" class="mt-4 transition-opacity duration-300 ease-in-out">
-      <CommentForm @comment-submitted="addComment" />
-      <Comment :comments="comments" />
-    </div>
-  </div>
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+  getInstruments()
+})
+</script>
+
+
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
 </template>
 
 <script>
 import Comment from './components/Comment.vue';
 import CommentForm from './components/CommentForm.vue';
 
-const app = Vue.createApp({
-    data() {
-        return {
-            showForm: false, // Controls the visibility of the form
-            name: "",
-            newComment: "",
-            comments: []
-        };
-    },
-    methods: {
-        toggleContent() {
-            this.showForm = !this.showForm; // Toggles form visibility
-        },
-        addComment() {
-            if (this.name && this.newComment) {
-                const timestamp = new Date().toLocaleString();
-                this.comments.push({
-                    name: this.name,
-                    text: this.newComment,
-                    timestamp
-                });
-                this.name = "";
-                this.newComment = "";
-            } else {
-                alert("Please enter your name and comment!");
-            }
-        }
-    }
-});
 
 app.mount("#appContainer");
 </script>
