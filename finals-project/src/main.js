0a4fa228a5bc78498c7c89c6d530fc479d6a88dd
Marianna Ellise Lama <153097209+mariannelama@users.noreply.github.com>
2025-03-03 05:55:18 +0000

    mm

--- finals-project/src/main.js
+++ finals-project/src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './css/style.css' // Ensure your styles are applied
-import CommentForm from './components/CommentForm.vue'
-import Comment from './components/Comment.vue'
+import CommentSection from './components/CommentSection.vue'
 
 // Create and mount the Vue app
 const app = createApp(App)
+
 // Register components globally (optional, but not needed if using local registration in App.vue)
-app.component('CommentForm', CommentForm)
-app.component('Comment', Comment)
+app.component('CommentSection', CommentSection)
+
 app.mount('#app')
